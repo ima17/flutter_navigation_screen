ca98584794908fdd7a8b2302159ff572160473cf
ima17 <ima.sudaraka@outlook.com>
2022-12-07 20:34:49 +0530

    Initial commit

---
+++ lib/configs/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // Common Colors
+  static const primaryColor = Color(0xFF31b768);
+  static const darkBlackColor = Color(0xFF000000);
+  static const lightWhiteColor = Color(0xFFFFFFFF);
+}
